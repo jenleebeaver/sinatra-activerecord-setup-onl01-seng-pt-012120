586cd591c514a58935134dc875d8c6f06f1da585
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-22 18:38:06 +0000

    Automatically backed up by Learn

--- db/migrate/20200422183117_create_dogs.rb
+++ db/migrate/20200422183117_create_dogs.rb
@@ -1,12 +1,19 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def up
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end
- end
+ #  def up
+ #    create_table :dogs do |t|
+ #      t.string :name
+ #      t.string :breed
+ #    end
+ # end
+ #
+ # def down
+ #   drop_table :dogs
+ # end
 
- def down
-   drop_table :dogs
+ def change #same as the up and down method (down method or rollback is implicit)
+   create_table :dogs do |t|
+     t.string :name
+     t.string :breed
+   end
  end
 end
